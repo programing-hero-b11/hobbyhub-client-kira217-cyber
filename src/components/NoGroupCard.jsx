4d64fe01fc55f1fe8e29a40eea6f1ffcf2e9e6ec
kira217-cyber <gamingkira217@gmail.com>
2025-05-22 18:35:22 +0600

    added all part

--- src/components/NoGroupCard.jsx
+++ src/components/NoGroupCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 const NoGroupCard = () => {
   return (
-    <div className="w-full bg-white dark:bg-gray-800 rounded-lg shadow p-6 text-center">
+    <div className="w-full rai bg-white dark:bg-gray-800 rounded-lg shadow p-6 text-center">
       <h2 className="text-2xl font-semibold text-gray-800 dark:text-white mb-3">
         No Groups Found
       </h2>
